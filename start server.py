cf183ab04f7da738ddfa62c4f97d95932122ad8c
LiathDevelopment <71844824+LiathDevelopment@users.noreply.github.com>
2021-03-19 18:25:07 +0000

    Quite a few changes
    The script needs to be properly organised still

--- start server.py
+++ start server.py
@@ -1,48 +1,62 @@
-from subprocess import PIPE
-import subprocess
-import threading
+from subprocess import PIPE, Popen
+from datetime import datetime
+from threading import Timer
+from shutil import copy
+from os import system
 import schedule
-import datetime
-import shutil
-import os
 
-start_cmd = 'cmd /c java -Xms512M -Xmx512M -Dhttp.proxyHost=betacraft.pl -cp minecraft-server.jar com.mojang.minecraft.server.MinecraftServer'
+start_cmd = "java -Xms512M -Xmx512M -Dhttp.proxyHost=betacraft.pl -cp minecraft-server.jar com.mojang.minecraft.server.MinecraftServer"
+server_config = {}
+
 
 def logger(string):
-    print("     " + getTimeString() + "  " + string)
+    now_hour = datetime.now().hour
+    now_minute = datetime.now().minute
+    now_second = datetime.now().second
+    if len(str(now_hour)) == 1: now_hour = "0" + str(now_hour)
+    if len(str(now_minute)) == 1: now_minute = "0" + str(now_minute)
+    if len(str(now_second)) == 1: now_second = "0" + str(now_second)
+    time = str(now_hour) + ":" + str(now_minute) + ":" + str(now_second)
 
-def getTimeString():
-    nowHour = datetime.datetime.now().hour
-    nowMinute = datetime.datetime.now().minute
-    nowSecond = datetime.datetime.now().second
-    if len(str(nowHour)) == 1:
-        nowHour = "0"+str(nowHour)
-    if len(str(nowMinute)) == 1:
-        nowMinute = "0"+str(nowMinute)
-    if len(str(nowSecond)) == 1:
-        nowSecond = "0"+str(nowSecond)
-    return str(nowHour)+":"+str(nowMinute)+":"+str(nowSecond)
+    print("     " + time + "  " + string)
+
+
+def getServerConfig():
+    with open('server.properties') as f:
+        for line in f:
+            if "=" in line:
+                name, value = line.split("=", 1)
+                server_config[name.strip()] = value.strip()
+
 
 def createBackup():
-    now = str(datetime.datetime.now())[:19]
-    now = now.replace(":","-")
+    now = str(datetime.now())[:19]
+    now = now.replace(":", "-")
     now = now.replace(" ", "_")
 
-    src_dir="server_level.dat"
-    dst_dir=".\\backups\\server_level_backup_"+str(now)+".dat"
-    shutil.copy(src_dir,dst_dir)
-    logger("Server backup created")
+    copy("server_level.dat", ".\\backups\\server_level_backup_" + str(now) + ".dat")
+    logger("Level backup created")
+
 
 def autoBackupThread():
     i = 1
-    threading.Timer(i, autoBackupThread).start()
+    Timer(i, autoBackupThread).start()
     schedule.run_pending()
 
-os.system('title Classic Revived 0.0.18a_02 Server')
-process = subprocess.Popen(start_cmd,stdin=PIPE,stdout=PIPE)
-schedule.every(6).hours.do(createBackup)
-autoBackupThread()
 
-while process.poll() is None:
-        output = process.stdout.readline()
-        print(output)
+def serverConsoleThread():
+    i = 1
+    Timer(i, serverConsoleThread).start()
+    while process.poll() is None:
+        server_output = process.stdout.readline()
+        print(server_output)
+
+
+getServerConfig()
+system("title Classic Revived 0.0.18a_02 - Server Port: " + server_config["port"])
+process = Popen("cmd /c " + start_cmd, stdin=PIPE, stdout=PIPE)
+schedule.every(6).hours.do(createBackup)
+# schedule.every(15).seconds.do(createBackup)
+logger("Scheduling backups")
+autoBackupThread()
+createBackup()
